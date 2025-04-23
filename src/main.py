5c4f62177cbcd6299a196da409815815b4b34487
Saiful Islam <saiful@co.design>
2025-04-23 15:48:29 +0600

    update

--- src/main.py
+++ src/main.py
@@ -13,6 +13,6 @@ app = FastAPI()
 """ Only uncomment below to create new tables, 
 otherwise the tests will fail if not connected
 """
-# Base.metadata.create_all(bind=engine)
+
 
 register_routes(app)
